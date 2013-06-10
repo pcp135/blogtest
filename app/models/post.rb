efee5b2f188b8ebf03040b46f2ae8843a8864b87
Phil <pcp135@gmail.com>
2013-06-10 01:51:02 -0400

    set dependent comments to be deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,6 @@ class Post < ActiveRecord::Base
   validates :title,   :presence => true,
                       :length => { :minimum => 5 }
                       
-  has_many :comments
+  has_many :commentsm :dependent => :destroy
   
 end
